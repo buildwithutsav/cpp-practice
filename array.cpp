3a24f29c8283b22e5456ef3df5738c0b0f1143b3
buildwithutsav <maharishiutsav@gmail.com>
2026-07-27 23:03:19 +0530

    Day 4:practiced more questions of array

--- array.cpp
+++ array.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 using namespace std;
-int main(){
+/*int main(){
     int i;
     int arr[4];
 
@@ -15,4 +15,95 @@ int main(){
 
     }
     return 0;
+}*/
+
+/* int main(){
+    int n , i ;
+    int arr[];
+
+    cout<<"Enter number of elements : ";
+    cin>>n;
+
+    for(i=0;i<n;i++){
+        cin>>arr[i];
+
+    }
+    for(i=0;i<n;i++){
+        cout<<arr[i]<<endl;
+    }
+
+    cout<<"OUTPUT:"
+
+}*/
+
+/*int main(){
+    int n , i  ;
+    
+
+    cout<<"Enter number of elements:";
+    cin>>n;
+
+    vector<int>arr(n);
+    cout<<"Enter elements";
+    for (i=0;i<n;i++){
+     
+     cin>>arr[i];
+     
+    }
+int largest = arr[0];
+
+for(i=1;i<n;i++){
+    if(arr[i]>largest){
+        largest = arr[i];
+    }
+}
+cout<<"Output: Largest Element = "<< largest << endl;
+return 0;
+
+}*/
+
+/*int main(){
+    int n,i;
+    cout<<"Enter number of Elements:";
+    cin>>n;
+
+    vector<int>arr(n);
+    cout<<"Enter Elements";
+    for(i=0;i<n;i++){
+        cin>>arr[i];
+        cout<<"Element on "<< i << " th index is "<< arr[i] << endl;
+
+    }
+    int largest = arr[0];
+    for(i=1;i<n;i++){
+        if(arr[i]>largest){
+            largest = arr[i];
+        }
+    }
+    int sec = arr[0];
+    for(i=1;i<n;i++){
+        if(arr[i]>sec && arr[i]<largest){
+            sec = arr[i];
+
+        }
+    }
+    cout<<"Second Largest Element is "<< sec;
+    return 0;
+}*/
+
+int main(){
+    int n ,i ;
+    cout<<"ENTER NUMBER OF ELEMENTS:";
+    cin>>n;
+
+    vector<int>arr(n);
+    for(i=0;i<n;i++){
+        cin>>arr[i];
+    }
+
+    cout<<"REVERSED ARRAY: ";
+    for(i=n-1;i>=0;i--){
+        cout<<arr[i]<<" ";
+    }
+    
 }
