8df980d2cfcae4f041357b07c3543afdcb50b47f
buildwithutsav <maharishiutsav@gmail.com>
2026-07-29 23:24:22 +0530

    Day 5:practiced string

---
+++ string.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <string>
+using namespace std; 
+/*int main(){
+    string name;
+
+    cout<<"Enter your name: ";
+    cin>>name;
+
+    cout<<"OUTPUT:"<<endl;
+    cout<<"YOUR ENTERED NAME IS - "<<name;
+
+    return 0;
+}*/
+
+/*int main(){
+    string name;
+
+    cout<<"Enter Your Name: ";
+    cin>>name;
+
+    cout<<"OUTPUT"<<endl;
+    cout<<"Length of your Name is: "<<name.length();
+
+    return 0;
+    
+}*/
+
+/*int main(){
+    int i, count=0;
+    string n;
+
+    cout<<"Enter a String: ";
+    getline(cin, n);
+
+    for(int i=0 ; i < n.length(); i++){
+
+        if(n[i]=='a'|| n[i] == 'e' || n[i] == 'i' || n[i] == 'o' || n[i]=='u' || n[i]=='A' || n[i]=='E' || n[i]=='I' || n[i]=='O' || n[i]=='U'){
+            count++;
+        }
+    }
+
+    cout<<"Number Of Vowels IN String: "<<count;
+    return 0;
+ 
+    
+}*/
+
+int main(){
+    string name;
+    int i ;
+
+    cout<<"Enter Your String: ";
+    getline(cin,name);
+
+    cout<<"Reversed String: ";
+    for(i= name.length() - 1; i>=0 ; i--){
+        cout<<name[i];
+    }
+
+    cout<<endl;
+
+    return 0;
+
+}
