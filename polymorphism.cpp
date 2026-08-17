921249ac42c796d518bf8a55c6a509abe29e5e25
buildwithutsav <maharishiutsav@gmail.com>
2026-08-17 22:07:53 +0530

    practiced runtime polymorphism

--- polymorphism.cpp
+++ polymorphism.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 using namespace std;
 
-class Calculator {
+/*class Calculator {
 public:
     int add(int a, int b) {
         return a + b;
@@ -22,6 +22,52 @@ int main() {
     cout << "Two integers: " << calc.add(10, 20) << endl;
     cout << "Three integers: " << calc.add(10, 20, 30) << endl;
     cout << "Two floats: " << calc.add(2.5f, 3.5f) << endl;
+
+    return 0;
+}*/
+
+#include <iostream>
+using namespace std;
+
+class Shape {
+public:
+    virtual void area() {
+        cout << "Area of shape" << endl;
+    }
+};
+
+class Rectangle : public Shape {
+public:
+    int length = 10;
+    int width = 5;
+
+    void area() override {
+        cout << "Area of Rectangle: "
+             << length * width << endl;
+    }
+};
+
+class Circle : public Shape {
+public:
+    float radius = 7;
+
+    void area() override {
+        cout << "Area of Circle: "
+             << 3.14 * radius * radius << endl;
+    }
+};
+
+int main() {
+    Shape* ptr;
+
+    Rectangle r;
+    Circle c;
+
+    ptr = &r;
+    ptr->area();
+
+    ptr = &c;
+    ptr->area();
 
     return 0;
 }
